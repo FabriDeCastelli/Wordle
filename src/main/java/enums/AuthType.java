2e5ce16b4dcf7e2b5020fd13cf949c3c0da2d971
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2023-07-27 16:41:10 +0200

    Created enum to manage authentication type.

---
+++ src/main/java/enums/AuthType.java
@@ -0,0 +1,16 @@
+package enums;
+
+/**
+ * AuthType is an enum that represents the type of authentication.
+ */
+public enum AuthType {
+    /**
+     * LOGIN represents the login authentication type.
+     */
+    Login,
+
+    /**
+     * REGISTER represents the register authentication type.
+     */
+    Register,
+}
