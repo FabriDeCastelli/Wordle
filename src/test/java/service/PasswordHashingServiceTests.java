26bcf3261dd3fa6aa97b9d5105e0becba00edcd1
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2023-08-10 00:04:17 +0200

    Changed encryption algorithm to SHA-256.

--- src/test/java/service/PasswordHashingServiceTests.java
+++ src/test/java/service/PasswordHashingServiceTests.java
@@ -1,7 +1,6 @@
 package service;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import client.service.PasswordHashingService;
