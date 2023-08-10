cb6721d91b9c3fc935c7f3f6e4c9564a347ed771
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2023-08-10 11:29:15 +0200

    Created register command as type of command.

---
+++ src/main/java/server/service/RegisterCommand.java
@@ -0,0 +1,51 @@
+package server.service;
+
+import java.io.ObjectOutputStream;
+import model.ServerResponse;
+import model.User;
+import model.UserRequest;
+import server.model.Command;
+
+/**
+ * Register command.
+ */
+public class RegisterCommand implements Command {
+
+    private final AuthenticationService authenticationService;
+    private final ObjectOutputStream out;
+
+    /**
+     * Constructor for RegisterCommand.
+     *
+     * @param authenticationService the authentication service
+     * @param out the output stream
+     */
+    public RegisterCommand(AuthenticationService authenticationService, ObjectOutputStream out) {
+        this.authenticationService = authenticationService;
+        this.out = out;
+    }
+
+    /**
+     * Handles a user request.
+     *
+     * @param userRequest the user request
+     * @return true if the user request was handled successfully
+     */
+    public boolean handle(UserRequest userRequest) {
+
+        final User user = userRequest.user();
+        final ServerResponse serverResponse;
+
+        if (user.getPasswordHash().isEmpty()) {
+            serverResponse = new ServerResponse(-1, "Password cannot be empty.");
+        } else if (authenticationService.getUserByUsername(user.getUsername()).isPresent()) {
+            serverResponse = new ServerResponse(-1, "User already registered.");
+        } else if (authenticationService.add(user)) {
+            serverResponse = new ServerResponse(0, "Registration successful.");
+        } else {
+            serverResponse = new ServerResponse(-1, "Registration failed.");
+        }
+
+        return sendResponse(out, serverResponse);
+    }
+}
