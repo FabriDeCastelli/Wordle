84bb9221d6db8ddbe8ad8ccadda942a6e485af43
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2023-08-12 15:06:39 +0200

    Changed signature of handle method to manage better errors. Removed useless method as now is used in the StreamHandler class.

--- src/main/java/server/model/Command.java
+++ src/main/java/server/model/Command.java
@@ -15,25 +15,10 @@ public interface Command {
      * Handles a user request.
      *
      * @param userRequest the user request
-     * @return true if the user request was handled successfully
+     * @return the server response
      */
-    boolean handle(@NotNull UserRequest userRequest);
+    ServerResponse handle(@NotNull UserRequest userRequest);
 
-    /**
-     * Writes a ServerResponse object on an output stream.
-     *
-     * @param out the output stream
-     * @param serverResponse the server response
-     * @return true if the response was sent successfully
-     */
-    default boolean sendResponse(ObjectOutputStream out, ServerResponse serverResponse) {
-        try {
-            out.writeObject(serverResponse);
-            out.flush();
-        } catch (IOException e) {
-            return false;
-        }
-        return true;
-    }
+
 
 }
