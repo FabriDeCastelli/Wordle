fb87ffcefd6011a12ee6b8d9ab097a24c5297109
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2023-08-14 00:07:43 +0200

    Added scheduled word extraction in the pool.

--- src/main/java/server/WordleServerMain.java
+++ src/main/java/server/WordleServerMain.java
@@ -7,15 +7,17 @@ import java.net.ServerSocket;
 import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import server.controller.RequestHandler;
 import server.controller.TerminationHandler;
+import server.service.WordExtractionService;
 
 /**
  * Communicates with WordleClientMain through a Persistent TCP connection.
  */
 public class WordleServerMain {
 
-    private static int PORT;
 
     /**
      * Main method for the WordleServer.
@@ -23,21 +25,30 @@ public class WordleServerMain {
     @SuppressWarnings("PMD")
     public static void main(String[] args) {
 
+        final int port;
+        final int wordDuration;
         try (final InputStream inputStream =
                      new FileInputStream("src/main/java/server/conf/server.properties")) {
             final Properties properties = new Properties();
             properties.load(inputStream);
-            PORT = Integer.parseInt(properties.getProperty("PORT"));
+            port = Integer.parseInt(properties.getProperty("PORT"));
+            wordDuration = Integer.parseInt(properties.getProperty("WORDDURATION"));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error reading properties file.");
             return;
         }
 
-        try (ServerSocket server = new ServerSocket(PORT)) {
+        try (ServerSocket server = new ServerSocket(port)) {
 
             final ExecutorService executorService = Executors.newCachedThreadPool();
             Runtime.getRuntime().addShutdownHook(
                     new TerminationHandler(2000, executorService, server)
+            );
+
+            final ScheduledExecutorService wordExtractionService =
+                    Executors.newSingleThreadScheduledExecutor();
+            wordExtractionService.scheduleAtFixedRate(
+                    new WordExtractionService(), 0, wordDuration, TimeUnit.MILLISECONDS
             );
 
             while (true) {
@@ -45,7 +56,7 @@ public class WordleServerMain {
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error creating socket.");
         }
 
     }
