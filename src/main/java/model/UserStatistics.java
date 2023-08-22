e9004e332c85831e299feeb6d0f7ee35b8e49d59
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2023-08-22 19:17:55 +0200

    Created class to handle statistics.

---
+++ src/main/java/model/UserStatistics.java
@@ -0,0 +1,102 @@
+package model;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a user's statistics on played games.
+ */
+public class UserStatistics implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private int gamesPlayed;
+    private int gamesWon;
+    private double gamesWonPercentage;
+    private int currentStreak;
+    private int longestStreak;
+    private final List<Integer> guessDistribution;
+
+
+    /**
+     * Constructor for the UserStatistics.
+     */
+    public UserStatistics() {
+        this.gamesPlayed = 0;
+        this.gamesWon = 0;
+        this.gamesWonPercentage = 0;
+        this.currentStreak = 0;
+        this.longestStreak = 0;
+        this.guessDistribution = new ArrayList<>();
+    }
+
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    /**
+     * Increments the number of games played and updates the games won percentage.
+     */
+    public void incrementGamesPlayed() {
+        this.gamesPlayed++;
+        if (gamesPlayed != 0) {
+            this.gamesWonPercentage = (double) gamesWon / gamesPlayed;
+        }
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    /**
+     * Increments the number of games won and updates the games won percentage.
+     */
+    public void incrementGamesWon() {
+        this.gamesWon++;
+        if (gamesPlayed != 0) {
+            this.gamesWonPercentage = (double) gamesWon / gamesPlayed;
+        }
+    }
+
+    public double getGamesWonPercentage() {
+        return gamesWonPercentage;
+    }
+
+
+    public int getCurrentStreak() {
+        return currentStreak;
+    }
+
+    /**
+     * Increments the current streak and updates the longest streak.
+     */
+    public void incrementCurrentStreak() {
+        this.currentStreak++;
+        if (currentStreak > longestStreak) {
+            this.longestStreak = currentStreak;
+        }
+    }
+
+    public int getLongestStreak() {
+        return longestStreak;
+    }
+
+
+    public List<Integer> getGuessDistribution() {
+        return guessDistribution;
+    }
+
+    /**
+     * Adds the number of trials to the guess distribution.
+     *
+     * @param trials the number of trials
+     */
+    public void addTrials(int trials) {
+        this.guessDistribution.add(trials);
+    }
+
+}
---
+++ src/main/java/server/service/UserStatisticsService.java
@@ -0,0 +1,71 @@
+package server.service;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Optional;
+import model.User;
+import model.UserStatistics;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * User statistics service.
+ */
+public class UserStatisticsService extends UserService {
+
+    /**
+     * Constructor for the UserStatisticsService.
+     */
+    public UserStatisticsService() {
+        super("src/main/java/server/conf/users.json");
+    }
+
+    /**
+     * Constructor for the UserStatisticsService.
+     *
+     * @param filePath the file path
+     */
+    public UserStatisticsService(String filePath) {
+        super(filePath);
+    }
+
+    /**
+     * Gets the user statistics with the given username.
+     *
+     * @param username the username
+     * @param userStatistics the user statistics
+     * @return true for success and false for failure
+     */
+    public synchronized boolean updateStatistics(
+            @NotNull String username, UserStatistics userStatistics) {
+        final Optional<User> user = getUserByUsername(username);
+        if (user.isEmpty()) {
+            throw new IllegalArgumentException("Cannot update statistics for a not stored user");
+        }
+
+        user.get().setUserStatistics(userStatistics);
+        users.put(username, user.get());
+        try (final FileWriter writer = new FileWriter(filePath)) {
+            gson.toJson(users, writer);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Gets the user statistics with the given username.
+     *
+     * @param username the username
+     * @return the user statistics for that user
+     */
+    public synchronized UserStatistics getStatistics(@NotNull String username) {
+
+        final Optional<User> user = getUserByUsername(username);
+        if (user.isEmpty()) {
+            throw new IllegalArgumentException("Cannot get statistics for a not stored user");
+        }
+        return user.get().getUserStatistics();
+    }
+
+
+}
