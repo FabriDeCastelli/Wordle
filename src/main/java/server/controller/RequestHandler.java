2a16a3ec9992e0eb3a029348342cfcddcea38b76
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2023-08-24 12:23:58 +0200

    Added logic to handle share commands as they are instance-dependents.

--- src/main/java/server/controller/RequestHandler.java
+++ src/main/java/server/controller/RequestHandler.java
@@ -3,6 +3,7 @@ package server.controller;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.MulticastSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +22,8 @@ import server.service.command.LogoutCommand;
 import server.service.command.PlayCommand;
 import server.service.command.RegisterCommand;
 import server.service.command.SendWordCommand;
+import server.service.command.ShareCommand;
+
 
 /**
  * Handles a requestType from a client.
@@ -30,8 +33,13 @@ public class RequestHandler implements Runnable, AutoCloseable {
     private final Socket socket;
     private final ObjectInputStream in;
     private final ObjectOutputStream out;
+    private final MulticastSocket multicastSocket;
     private static final Map<RequestType, Command> commandMap;
+    private final Command shareCommand;
 
+    /*
+      Static initializer for instance independent Commands.
+     */
     static {
         commandMap = new HashMap<>();
         final AuthenticationService authenticationService = new AuthenticationService();
@@ -50,12 +58,16 @@ public class RequestHandler implements Runnable, AutoCloseable {
     /**
      * Constructor for RequestHandler.
      *
-     * @param socket the socket to accept
+     * @param socket          the socket to accept
+     * @param multicastSocket the multicast socket
      */
-    public RequestHandler(@NotNull Socket socket) throws IOException {
+    public RequestHandler(@NotNull Socket socket, MulticastSocket multicastSocket)
+            throws IOException {
         this.socket = socket;
         this.out = new ObjectOutputStream(socket.getOutputStream());
         this.in = new ObjectInputStream(socket.getInputStream());
+        this.multicastSocket = multicastSocket;
+        this.shareCommand = new ShareCommand(multicastSocket);
     }
 
     @Override
@@ -67,7 +79,15 @@ public class RequestHandler implements Runnable, AutoCloseable {
                 break;
             }
 
-            final Command command = commandMap.get(userRequest.get().requestType());
+            final RequestType requestType = userRequest.get().requestType();
+
+            Command command;
+            if (RequestType.SHARE == requestType) {
+                command = shareCommand;
+            } else {
+                command = commandMap.get(requestType);
+            }
+
             if (command == null) {
                 throw new IllegalStateException("Command not found.");
             }
@@ -91,6 +111,7 @@ public class RequestHandler implements Runnable, AutoCloseable {
         this.in.close();
         this.out.close();
         this.socket.close();
+        this.multicastSocket.close();
     }
 
 
