b660620d68229e716b1abab90e7746615fb079ee
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2023-08-27 13:23:00 +0200

    Changed signature of requests as the username was useless except for the authentication operations.

--- src/main/java/model/Request.java
+++ src/main/java/model/Request.java
@@ -2,25 +2,23 @@ package model;
 
 import java.io.Serializable;
 import model.enums.RequestType;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Represents a user requestType.
  */
 public record Request(
-        RequestType requestType,
-        String username,
+        @NotNull RequestType requestType,
         Object data) implements Serializable {
 
     /**
      * Constructor for the Request.
      *
      * @param requestType the request type
-     * @param username the name of the user sending the request
      */
-    public Request(RequestType requestType, String username) {
-        this(requestType, username, null);
+    public Request(RequestType requestType) {
+        this(requestType, null);
     }
-
 
 
 }
